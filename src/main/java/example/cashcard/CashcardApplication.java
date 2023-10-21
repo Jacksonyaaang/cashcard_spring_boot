0bec3b3c0088ce7b7640d1b77e09251f3789177b
jackson yang <jiaxuan.yang@grenoble-inp.org>
2023-10-21 19:44:58 +0200

    test for the get endpoint

--- src/main/java/example/cashcard/CashcardApplication.java
+++ src/main/java/example/cashcard/CashcardApplication.java
@@ -1,7 +1,15 @@
 package example.cashcard;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class CashcardApplication {
@@ -10,4 +18,6 @@ public class CashcardApplication {
 		SpringApplication.run(CashcardApplication.class, args);
 	}
 
+
 }
+
