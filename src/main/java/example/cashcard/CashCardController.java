f26cb2f0369832d47097e4adf85821c966c81bc0
jackson yang <jiaxuan.yang@grenoble-inp.org>
2023-10-29 21:37:32 +0100

    finish the post method to create a new cashcard and test it

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,20 +1,21 @@
 package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
     private CashCardRepository cashCardRepository;
-    //constructor
+    //constructor --> to show di in the signature
     public CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }
+    //the controller method to handle the GET request
     @GetMapping("/{cardid}")
     public ResponseEntity<CashCard> findId(@PathVariable long cardid){
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(cardid);
@@ -24,4 +25,14 @@ public class CashCardController {
             return ResponseEntity.notFound().build();
         }
     }
+    //the POST request handler (controller)
+    @PostMapping
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb){
+//        return null; //when return null, spring will return automatically the status code 200
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        //to build the location of the new cashcard
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
 }
