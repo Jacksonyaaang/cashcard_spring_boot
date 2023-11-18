58c37f07755426b80c108ffbb14a1376bb626e55
jackson yang <jiaxuan.yang@grenoble-inp.org>
2023-11-18 13:16:00 +0100

    avancer

--- src/main/java/example/cashcard/CashcardApplication.java
+++ src/main/java/example/cashcard/CashcardApplication.java
@@ -1,15 +1,8 @@
 package example.cashcard;
 
-import jakarta.websocket.server.PathParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class CashcardApplication {
